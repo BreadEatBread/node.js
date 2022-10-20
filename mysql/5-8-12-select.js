3a929437833fab82ca549545a8f36c7b34297b72
BreadEatBread <woong4252@gmail.com>
2022-10-20 11:54:39 +0900

    10.20.22 12:00

--- mysql/5-8-12-select.js
+++ mysql/5-8-12-select.js
@@ -2,9 +2,9 @@ const express = require('express')
 const mysql = require('mysql2');
 const app = express()
 
-require('dotenv').config(); //yarn add dotenv
+require('dotenv').config({path: 'mysql/.env'}); //yarn add dotenv
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.DB_USER,
@@ -14,17 +14,29 @@ const connection = mysql.createConnection({
 
 let body = '<h1>single row</h1>';
 connection.query(
-  'SELECT * FROM topic WHERE id = 1',
-  function(err, results, fields) {
+  'SELECT * FROM topic WHERE id = ?', [1], 
+  (err, results, fields) => {
+    console.log(err, results, fields);
     let row = results[0];
+    body += '<h2>' + row.title + '</h2>';
     body += row.description;
-    res.send(body);
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
   }
 );
 
-
+body += '<h1>multi row</h1>';
+connection.query(
+  'SELECT * FROM topic',
+  (err, results, fields) => {
+    console.log(err, results, fields);
+    // for( var i = 0; i < results.length; i++){
+    // let row = results[i];
+  for(let row of results){
+    body += '<h2>' + row.title + '</h2>';
+    body += row.description;
+    }
+    res.send(body);
+  }
+);
 
 //     $result = mysqli_query($conn, $sql);
 //     $row = mysqli_fetch_array($result);
